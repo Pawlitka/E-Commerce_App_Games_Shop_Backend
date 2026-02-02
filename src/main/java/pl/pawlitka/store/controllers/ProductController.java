340bedabe52e0fc3fbec16866ad710bd47be9d73
Pawlitka <pawel.kapusta2004@gmail.com>
2026-02-02 21:56:32 +0100

    create post createProduct, put and delete mappers for product

--- src/main/java/pl/pawlitka/store/controllers/ProductController.java
+++ src/main/java/pl/pawlitka/store/controllers/ProductController.java
@@ -1,17 +1,16 @@
 package pl.pawlitka.store.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import pl.pawlitka.store.dtos.ProductDto;
-import pl.pawlitka.store.dtos.UserDto;
 import pl.pawlitka.store.entities.Product;
 import pl.pawlitka.store.mappers.ProductMapper;
+import pl.pawlitka.store.repositories.CategoryRepository;
 import pl.pawlitka.store.repositories.ProductRepository;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -20,6 +19,7 @@ public class ProductController {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public List<ProductDto> getAllProducts(
@@ -41,6 +41,64 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriBuilder
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if(category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductDto productDto
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if(category == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productMapper.update(productDto, product);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        return ResponseEntity.ok(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id
+    ) {
+        var product = productRepository.findById(id).orElse(null);
+
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
+
     }
 
 }
--- src/main/java/pl/pawlitka/store/mappers/ProductMapper.java
+++ src/main/java/pl/pawlitka/store/mappers/ProductMapper.java
@@ -2,6 +2,7 @@ package pl.pawlitka.store.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import pl.pawlitka.store.dtos.ProductDto;
 import pl.pawlitka.store.entities.Product;
 
@@ -9,4 +10,9 @@ import pl.pawlitka.store.entities.Product;
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }
