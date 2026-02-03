23da11fe63d8ae913d0311f85a6cb68f2e2c5127
Pawlitka <pawel.kapusta2004@gmail.com>
2026-02-03 23:59:25 +0100

    add query for single game details

--- src/main/java/pl/pawlitka/store/repositories/GameDetailsRepository.java
+++ src/main/java/pl/pawlitka/store/repositories/GameDetailsRepository.java
@@ -1,15 +1,18 @@
 package pl.pawlitka.store.repositories;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.pawlitka.store.entities.GameDetails;
-import pl.pawlitka.store.entities.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface GameDetailsRepository extends JpaRepository<GameDetails, Long> {
     @Query(value = "SELECT g FROM GameDetails g WHERE g.active = TRUE")
     List<GameDetails> findAllActive();
+
+    @Query(value = "SELECT g FROM GameDetails g WHERE g.active = TRUE AND g.id = :id ORDER BY g LIMIT 1")
+    Optional<GameDetails> findById(@Param("id") Long id);
 }
